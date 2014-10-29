d9682d892f8318a59508344ec5955cc8fdfbbf68
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-10-29 13:40:42 -0400

    2

--- HW4/urrsched.c
+++ HW4/urrsched.c
@@ -81,7 +81,7 @@ static ssize_t urrsched_call(struct file *file, const char __user *buf,
 	}
     //firstcall
     if(firstCall){//make first call copy
-        user_rr_sched_class = kmalloc(sizeof(struct sched_class)+1, GFP_ATOMIC);
+        user_rr_sched_class = kmalloc(sizeof(call_task->sched_class)+1, GFP_ATOMIC);
         if(&user_rr_sched_class == NULL){
             preempt_enable(); 
             return -ENOSPC;
