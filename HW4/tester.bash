9693fd08e0cbe51d57361573d11a9b9127f4d02d
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-10-31 15:37:18 -0400

    autocommit on change

--- HW4/tester.bash
+++ HW4/tester.bash
@@ -5,6 +5,9 @@ cd ../HW3
 cd ../HW4
 ../HW3/call_create event_create HW4 #create the event to wait on, the callers will make a call to wait on event 0
 sudo ./call_create sched_uwrr 10 &
+sleep 1
 sudo ./call_create sched_uwrr 10 &
+sleep 1
 sudo ./call_create sched_uwrr 10 &
+sleep 1
 ../HW3/call_create event_signal 0 
