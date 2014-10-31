cb12d3dd7b0b5b23800f47a606fd8aa442cec17a
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-10-31 11:03:19 -0400

    autocommit on change

--- HW4/urrsched.c
+++ HW4/urrsched.c
@@ -95,13 +95,14 @@ static ssize_t urrsched_call(struct file *file, const char __user *buf, size_t c
 	}
     //firstcall
     if(firstCall){//make first call copy
+        user_rr_sched_class = kmalloc(sizeof(*(call_task->sched_class) ), GFP_ATOMIC);
         printk(KERN_DEBUG "urrsched: urrsched begin firstCall logic PID %i \n", call_task->pid);
         task_tick_orig = call_task->sched_class->task_tick;
         get_rr_interval_orig = call_task->sched_class->get_rr_interval;
         memcpy(&user_rr_sched_class, call_task->sched_class, sizeof( *(call_task->sched_class))+1 );        
         user_rr_sched_class.task_tick = urr_task_tick;
         user_rr_sched_class.get_rr_interval = urr_get_rr_interval;
-        printk(KERN_DEBUG "urrsched: for PID %i task_tick_orig: %p get_rr_interval_orig: %p\n", call_task->pid, task_tick_orig, get_rr_interval_orig);
+        //printk(KERN_DEBUG "urrsched: for PID %i task_tick_orig: %p get_rr_interval_orig: %p\n", call_task->pid, task_tick_orig, get_rr_interval_orig);
         firstCall = 0;
     }
     ///Here we set the call task to use our new sched class
