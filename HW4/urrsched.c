773394d9868d52aadbd336b18579ed4b8a9f15a9
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-10-31 13:57:54 -0400

    autocommit on change

--- HW4/urrsched.c
+++ HW4/urrsched.c
@@ -99,7 +99,7 @@ static ssize_t urrsched_call(struct file *file, const char __user *buf, size_t c
 	}
     else{
         //we have a good call
-        int convstr = kstrtoint( (callbuf[sizeof(URRSCHED_CALL) + 1]), 10, callbuf_param1 );
+        int convstr = kstrtoint( &(callbuf[sizeof(URRSCHED_CALL) + 1]), 10, callbuf_param1 );
         if (convstr != 0) {
             printk(KERN_DEBUG "urrsched: call %s will return %s the parameter was not acceptable\n", callbuf, respbuf);
             preempt_enable(); 
