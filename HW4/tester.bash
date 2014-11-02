3344f6576e7b321c3a0b3cb6b6773c4232399c22
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-11-02 17:19:29 -0500

    autocommit on change

--- HW4/tester.bash
+++ HW4/tester.bash
@@ -10,19 +10,18 @@ echo Prepart to create event and start data collecting:
 sleep 5
 call_create_HW3 event_create HW4 #create the event to wait on, the callers will make a call to wait on event 0
 sleep 2
-sudo ./call_create sched_uwrr 10 &
 sudo ./call_create sched_uwrr 50 &
 sudo ./call_create sched_uwrr 100 &
-#sudo ./call_create sched_uwrr 3 &
-#sudo ./call_create sched_uwrr 5 &
-#sudo ./call_create sched_uwrr 8 &
-#sudo ./call_create sched_uwrr 2 &
-#sudo ./call_create sched_uwrr 6 &
-#sudo ./call_create sched_uwrr 7 &
-#sudo ./call_create sched_uwrr 9 &
-#sudo ./call_create sched_uwrr 4 &
-#sudo ./call_create sched_uwrr 10 &
-#sudo ./call_create sched_uwrr 1 &
+sudo ./call_create sched_uwrr 3 &
+sudo ./call_create sched_uwrr 5 &
+sudo ./call_create sched_uwrr 8 &
+sudo ./call_create sched_uwrr 2 &
+sudo ./call_create sched_uwrr 6 &
+sudo ./call_create sched_uwrr 7 &
+sudo ./call_create sched_uwrr 9 &
+sudo ./call_create sched_uwrr 4 &
+sudo ./call_create sched_uwrr 10 &
+sudo ./call_create sched_uwrr 1 &
 sleep 1
 sleep 5
 echo Prepare to release waiters
