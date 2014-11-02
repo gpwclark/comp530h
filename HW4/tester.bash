267aa1c0d6a7b332a6873dd6f29216f304fe1e20
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-11-02 12:45:20 -0500

    autocommit on change

--- HW4/tester.bash
+++ HW4/tester.bash
@@ -3,12 +3,20 @@
 cd ../HW3
 ./insertnew.bash
 cd ../HW4
-../HW3/call_create event_create HW4 #create the event to wait on, the callers will make a call to wait on event 0
+echo
+echo
+echo Prepart to create event and start data collecting:
 sleep 5
-sudo ./call_create sched_uwrr 10 &
+call_create_HW3 event_create HW4 #create the event to wait on, the callers will make a call to wait on event 0
+sleep 2
+sudo ./call_create sched_uwrr 1 &
 sleep 1
 sudo ./call_create sched_uwrr 10 &
 sleep 1
-sudo ./call_create sched_uwrr 10 &
+sudo ./call_create sched_uwrr 100 &
 sleep 1
+sudo ./call_create sched_uwrr 1000 &
+sleep 5
+echo Prepare to release waiters
+echo
 ../HW3/call_create event_signal 0 
