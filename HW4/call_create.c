93b97dae5283bc357dcd8346e01947ca7cb64e17
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-11-02 13:31:45 -0500

    autocommit on change

--- HW4/call_create.c
+++ HW4/call_create.c
@@ -72,7 +72,7 @@ int main (int argc, char* argv[])
 	do_syscall(argS);
 	fprintf(stdout, "Module urrsched returns %s to PID %d\n", resp_buf, my_pid);
     //For testing busy wait
-    int counter = 0;
+    long long int counter = 0;
     while(ereturn != -1){
         counter++;
         if(counter == CALLERCYCLES)
