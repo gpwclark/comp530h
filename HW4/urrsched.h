b33cad73e2d65c809cf0084ddbbf494aba4b1dab
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-11-02 23:37:54 -0500

    autocommit on change

--- HW4/urrsched.h
+++ HW4/urrsched.h
@@ -9,8 +9,8 @@
 #define URRSCHED_CALL "sched_uwrr"
 #define MAX_URR_PS 1000
 #define TENMS ( (10 * HZ ) / 1000)
-#define CALLERCYCLESL ((unsigned long long int) 100000000)
-//#define CALLERCYCLES 500000000
+//#define CALLERCYCLESL ((unsigned long long int) 100000000)
+#define CALLERCYCLES 500000000L
 char dir_name[] = "urrsched";
 char file_name[] = "call";
 
