c9bce0b4eb2aae83ca31d9be9563b62efcf04831
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-11-04 15:13:41 -0500

    autocommit on change

--- HW4/datagen.py
+++ HW4/datagen.py
@@ -14,23 +14,23 @@ with open('kernel.log', 'r') as csvfile:
                 except ValueError:
                     pass
             data.append(np.array(parsedrow))
-
 #make data into numpy array
 data = np.array(data)
 #print('Example input string that we are parsing:')
 #print('urr_task_tick PID 3482 with weight 1 timeslice 10 RUNtime 162985252280 ACTUALtime 309785010174 tick_count 16751')
 #PID WEIGHT TIMESLICE RUNTIME ACTUALTIME TICK
-print(data)
 
 #plot the data
 starttime_ns = data[0][4]
 x = []
 y = []
 xlabels = ['Time']
+#data = np.sort(data, axis=0)
 
+print(data)
 for line in data:
     x.append( (line[4] - starttime_ns) * (10**-6) ) # the time is added to the x
-    y.append(line[1])
+    y.append(line[1] - lowestPID)
 
 print(x[0])
 print(y[0])
@@ -40,7 +40,7 @@ plt.plot(x, y, 'ro')
 # You can specify a rotation for the tick labels in degrees or with keywords.
 #plt.xticks(x, xlabels, rotation='vertical')
 plt.title('Execution of User Round Robin Processes over time')
-plt.ylabel('PID')
+plt.ylabel('Process Weight')
 plt.xlabel('Time in ms')
 # Pad margins so that markers don't get clipped by the axes
 plt.margins(0.2)
