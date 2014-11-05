b95b4b9ff49db2f5a088fb9735ba40995ae0174a
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-11-05 14:45:36 -0500

    report added

--- HW4/datagen.py
+++ HW4/datagen.py
@@ -14,19 +14,17 @@ with open('kernel.log', 'r') as csvfile:
                 except ValueError:
                     pass
             data.append(np.array(parsedrow))
-#make data into numpy array
-data = np.array(data)
 #print('Example input string that we are parsing:')
 #print('urr_task_tick PID 3482 with weight 1 timeslice 10 RUNtime 162985252280 ACTUALtime 309785010174 tick_count 16751')
 #PID WEIGHT TIMESLICE RUNTIME ACTUALTIME TICK
 
+data = np.array(data)
 #plot the data
 starttime_ns = data[0][4]
-print(data[1][4] - starttime_ns)
+#print(data[1][4] - starttime_ns)
 x = []
 y = []
 xlabels = ['Time']
-#data = np.sort(data, axis=0)
 
 #print(data)
 for line in data:
@@ -47,4 +45,43 @@ plt.xlabel('Time in ms')
 plt.margins(0.2)
 # Tweak spacing to prevent clipping of tick-labels
 plt.subplots_adjust(bottom=0.15)
-plt.show()
+#plt.show()
+
+#do more data analysis
+#print(data[0])
+#data.sort(axis=0) # put in order of PID
+sort_PID = data[data[:,0].argsort()]
+#print(data[0])
+split_by_PID ={}
+last_PID = 0
+
+for i, entry in enumerate(sort_PID):
+    if(last_PID != entry[0]):
+        split_by_PID[entry[0]] = []
+    split_by_PID[entry[0]].append(entry)
+    last_PID = entry[0]
+
+for key, entry in split_by_PID.items():
+    #each entry is a set of arrays with unique PID
+    entry = np.array(entry)
+    split_by_PID[key] = entry[entry[:,5].argsort()]
+print("SORTED into dict by pid in tick order\n")
+print("Here are the final tick prints for each PID")
+final_ticks = []
+first_ticks = []
+first_ticks.append(['PID', 'WEIGHT', 'TIMESLICE(MS)' ,'RUNTIME(NS)' ,'ACTUALTIME(NS)','TICK', 'STARTTIME(NS)'])
+final_ticks.append(['PID', 'WEIGHT', 'TIMESLICE(MS)' ,'RUNTIME(NS)' ,'ACTUALTIME(NS)','TICK', 'STARTTIME(NS)'])
+for key, entry in split_by_PID.items():
+    final_ticks.append(entry[-1])
+    first_ticks.append(entry[0])
+    print(entry[-1])
+
+with open('first_vals.csv', 'w', newline='') as fp:
+    a = csv.writer(fp, delimiter=',')
+    data = first_ticks
+    a.writerows(data)
+
+with open('final_vals.csv', 'w', newline='') as fp:
+    a = csv.writer(fp, delimiter=',')
+    data = final_ticks
+    a.writerows(data)
