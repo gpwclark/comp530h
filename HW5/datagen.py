3b44b0544538b7379f06834c88254b82edfa1b2d
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-11-25 15:13:12 -0500

    Added new files

--- HW5/datagen.py
+++ HW5/datagen.py
@@ -3,6 +3,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import sys
+def make_plot(data, index):
+    data = np.array(data)
+    x = []
+    y = []
+    for line in data:
+        x.append(line[index]) # the time is added to the x
+        y.append(line[-1])
+
+    x = np.array(x)
+    y = np.array(y)
+    fig = plt.figure()
+    ax = fig.add_subplot(2,1,1)
+    fig.suptitle('Page faults and the respective handling time ')
+    fig.add_axes(ylabel='Fault Time in NS')
+    fig.add_axes(xlabel='Page Offset')
+    fig.subplots_adjust(bottom=0.15)
+    line, = ax.plot(x,y, 'ro')
+    ax.set_yscale('log')
+    plt.show()
+
+
+
 data = []
 with open(sys.argv[1], 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -18,33 +40,7 @@ with open(sys.argv[1], 'r') as csvfile:
 #print('Example input string that we are parsing:')
 #print('Nov 24 21:44:24 localhost kernel: vmlogger: MM c17a3e40 PAGE 738767 PAGE_OFFSET 0 PFN 494180 TIME 814')
 #MM PAGE PAGE_OFFSET PFN TIME
-
-data = np.array(data)
-#plot the data
-#time_ns = data[][4]
-#print(data[1][4] - time_ns)
-x = []
-y = []
-xlabels = ['Page Offset']
-#print(data[0])
-for line in data:
-    x.append(line[2]) # the time is added to the x
-    y.append(line[-1])
-
-#print(x[0])
-#print(y[0])
-x = np.array(x)
-y = np.array(y)
-plt.plot(x, y, 'ro')
-# You can specify a rotation for the tick labels in degrees or with keywords.
-#plt.xticks(x, xlabels, rotation='vertical')
-
-plt.title('Execution of User Round Robin Processes over time')
-plt.ylabel('Fault Time in NS')
-plt.xlabel('Page Offset')
-# Pad margins so that markers don't get clipped by the axes
-plt.margins(0.2)
-# Tweak spacing to prevent clipping of tick-labels
-plt.subplots_adjust(bottom=0.15)
-plt.show()
-
+print(data[0])
+make_plot(data, 0) # this is x logical page
+make_plot(data, 1) # this is x page_offset
+make_plot(data, 2) # this is x PFN
